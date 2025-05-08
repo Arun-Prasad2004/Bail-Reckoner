067b3b1c608f50643537a7f83aaec981c013ad78
Arun-Prasad2004 <arunprasad839@gmail.com>
2025-05-08 20:47:52 +0530

    bail_re

--- Bail Reckoner website/website/src/App.jsx
+++ Bail Reckoner website/website/src/App.jsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { useEffect, useState } from "react";
 import 'leaflet/dist/leaflet.css';
 
